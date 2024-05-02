150a4e63f6c6fd3723bebd093673647ecb5e6469
kreker2112 <kreker2112@gmail.com>
2024-05-03 00:19:09 +0300

    revert to normal

---
+++ src/layouts/fundingsLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <Navbar />
+    <div class="currency-layout">
+        <slot></slot>
+    </div>
+    <FooterComponent />
+</template>
+
+<script setup lang="ts">
+import Navbar from '@/components/NavBar.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
+</script>
+
+<style scoped lang="scss">
+.currency-layout {
+    padding-top: 80px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    text-align: center;
+    width: 100%;
+}
+</style>
