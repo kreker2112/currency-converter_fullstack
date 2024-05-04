ea83f10dddab33c474ee9ad6d31f2b22e7e92b32
kreker2112 <kreker2112@gmail.com>
2024-05-04 15:31:42 +0300

    some changes

--- src/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    // Другие опции конфигурации...
-    define: {
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true),
-    },
-});
--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,16 @@ const generalVarsFiles = ['/assets/scss/_variables.scss'];
 
 export default defineConfig({
     transpileDependencies: true,
+    chainWebpack: (config) => {
+        config.plugin('define').tap((definitions) => {
+            Object.assign(definitions[0], {
+                __VUE_OPTIONS_API__: JSON.stringify(true),
+                __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+                __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+            });
+            return definitions;
+        });
+    },
     css: {
         loaderOptions: {
             scss: {
